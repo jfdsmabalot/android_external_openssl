083bde011086064785f9971b9ca7bd3bd777f436
jfdsmabalot <jfdsmabalot@gmail.com>
2013-09-29 18:41:13 +0800

    4.9 Toolchain Patch

---
+++ android-config.mk
@@ -0,0 +1,31 @@
+#
+# These flags represent the build-time configuration of OpenSSL for android
+#
+# The value of $(openssl_cflags) was pruned from the Makefile generated
+# by running ./Configure from import_openssl.sh.
+#
+# This script performs minor but required patching for the Android build.
+#
+
+LOCAL_CFLAGS += $(openssl_cflags)
+
+LOCAL_CFLAGS := $(filter-out -DTERMIO, $(LOCAL_CFLAGS))
+
+ifeq ($(HOST_OS),windows)
+LOCAL_CFLAGS := $(filter-out -DDSO_DLFCN -DHAVE_DLFCN_H,$(LOCAL_CFLAGS))
+endif
+
+# Directories
+LOCAL_CFLAGS += \
+  -DOPENSSLDIR="\"/system/lib/ssl\"" \
+  -DENGINESDIR="\"/system/lib/ssl/engines\""
+
+# Intentionally excluded http://b/7079965
+LOCAL_CFLAGS := $(filter-out -DZLIB, $(LOCAL_CFLAGS))
+
+# OpenSSL isn't ready to be built with strict aliasing, and fixing it without
+# breaking both API and ABI is hard
+LOCAL_CFLAGS += -fno-strict-aliasing
+
+# Debug
+# LOCAL_CFLAGS += -DCIPHER_DEBUG
